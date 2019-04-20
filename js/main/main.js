cc321b3deb3e3f84ee4375cbb7f1109ae52c71c7
Ashik Iqbal <45705955+air2r@users.noreply.github.com>
2019-04-20 11:06:05 +0600

    Add files via upload

---
+++ js/main/main.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+        $('.subimt.subimit1').click(function(){
+        $('.display1').hide();
+        $('.active2').addClass('active');
+        $('.display2').css('display','block');
+            
+      
+
+   });
+    
+        $('.add_mor_cacancy_four').click(function(){
+        $('.display3').css('display','block');
+        $('.active3').addClass('active');
+         $('.display2').hide('');
+            
+
+
+   });
+    
+        $('.add_mor_cacancy_three').click(function(){
+        $('.display4').css('display','block');
+        $('.active4').addClass('active');
+        $('.display3').hide('');
+            
+
+
+   });
+    
+    
+
+
+
+});
